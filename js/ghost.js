90587752013d28eed9aba1a8e2e674570879ccc7
Kellen <k@8labs.com>
2014-01-18 13:49:20 -0800

    adding the sub module for the actual public portion of the blog. Checking in initial bits.

---
+++ js/ghost.js
@@ -0,0 +1,24 @@
+﻿//a ghost in effect when scrolling past a section
+
+//when we scroll, check positions
+$(window).scroll(ghost);
+
+//on document ready, show any that are already visible
+$(document).ready(ghost);
+
+//adds the class when the element is visible (or is past)
+function ghost() {
+    //loop ahoy
+    $('.ghost').not('.ghost-in').each(function (i) {
+
+        var top = $(this).position().top;
+        var pos = $(window).scrollTop() + $(window).height();
+
+        //if it's visible at all, fade it in
+        if (pos > top) {
+            $(this).addClass('ghost-in');
+        }
+    });
+}
+
+
